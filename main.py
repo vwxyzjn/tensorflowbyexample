add2f4ee285e2a25f2a840d12903b38b9e3bd052
Costa Huang <costa.huang@outlook.com>
2019-03-11 15:22:56 -0400

    Master # Auto generate jupyter notebook

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
-template = """
-{{
- "cells": [
-  {{
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {{}},
-   "outputs": [
-    {{
-     "name": "stdout",
-     "output_type": "stream",
-     "text": {}
-    }}
-   ],
-   "source": {}
-  }}
- ],
- "metadata": {{
-  "kernelspec": {{
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  }},
-  "language_info": {{
-   "codemirror_mode": {{
-    "name": "ipython",
-    "version": 3
-   }},
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.5"
-  }}
- }},
- "nbformat": 4,
- "nbformat_minor": 2
-}}
-"""
+import nbformat
+import argparse
+import os
+    
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser(description="convert python files to jupyter notebooks")
+    ap.add_argument("-d", help="examples directory", default="examples")
+    args = ap.parse_args()
+    
+    for dirpath, dirnames, filenames in os.walk("examples"):
+        # if we reach the leaf directory
+        if not dirnames:
+            for fn in filenames:
+                extension = fn.split('.')[-1]
+                file_title = fn.rstrip(extension)
+                if extension == 'py':
+                    with open(os.path.join(dirpath, fn), 'r') as f:
+                        code = f.read()
+                elif extension == 'sh':
+                    with open(os.path.join(dirpath, fn), 'r') as f:
+                        temp_output = f.read()
+                    real_ourputs = False
+                    outputs = []
+                    for line in temp_output.split('\n'):
+                        if line.startswith("$"):
+                            real_ourputs = True
+                            continue
+                        if real_ourputs:
+                            outputs += [line]
+                    output = "\n".join(outputs)
+                            
+
+            nb = nbformat.v4.new_notebook()
+            code_cell = nbformat.v4.new_code_cell(code)
+            code_cell.outputs += [nbformat.v4.new_output("stream", text=output)]
+            nb.cells += [code_cell]
+            with open(os.path.join(dirpath, file_title+"ipynb"), 'w') as f:
+                nbformat.write(nb, f)
