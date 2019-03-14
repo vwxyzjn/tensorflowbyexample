977cb0663465809b6a4b689cf055e4996c5d9b5b
Costa Huang <costa.huang@outlook.com>
2019-03-14 01:29:15 -0400

    Master # Update docs

--- examples/matrix_multiplication/matrix_multiplication.py
+++ examples/matrix_multiplication/matrix_multiplication.py
@@ -1,7 +1,24 @@
 import tensorflow as tf
-# Use eager execution to embrace the upcoming tensorflow 2.0.
+# Taken from https://www.tensorflow.org/guide/eager
+# Eager execution works nicely with NumPy. NumPy operations accept tf.Tensor arguments. 
+# TensorFlow math operations convert Python objects and NumPy arrays to tf.Tensor objects. 
+# The tf.Tensor.numpy method returns the object's value as a NumPy ndarray.
 tf.enable_eager_execution()
+a = tf.constant([
+    [1, 2],
+    [3, 4]]
+)
+print(a)
 
-x = tf.constant(3)
-m = tf.multiply(x, x)
-print("hello world {}".format(m))
+# Broadcasting support
+b = tf.add(a, 1)
+print(b)
+
+# Operator overloading is supported
+print(a * b)
+
+# Use NumPy values
+import numpy as np
+
+c = np.multiply(a, b)
+print(c)
