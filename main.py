581cd9b528806f1ae016711bed5e4e03915c8dfe
Costa Huang <costa.huang@outlook.com>
2019-03-13 23:10:27 -0400

    Master # Add script to automatically build it.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import nbformat
 import argparse
 import os
-    
+import subprocess
+import shutil
+
+# https://stackoverflow.com/questions/431684/how-do-i-change-directory-cd-in-python
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, newPath):
+        self.newPath = os.path.expanduser(newPath)
+
+    def __enter__(self):
+        self.savedPath = os.getcwd()
+        os.chdir(self.newPath)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.savedPath)
+
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description="convert python files to jupyter notebooks")
     ap.add_argument("-d", help="examples directory", default="examples")
     args = ap.parse_args()
     
+    # 1. Create Jupyter Notebooks.
     for dirpath, dirnames, filenames in os.walk("examples"):
         # if we reach the leaf directory
         if not dirnames:
@@ -36,3 +52,23 @@ if __name__ == '__main__':
             nb.cells += [code_cell]
             with open(os.path.join(dirpath, file_title+"ipynb"), 'w') as f:
                 nbformat.write(nb, f)
+
+    # 2. Generate Docs and paste them to the current folder.
+    # https://stackoverflow.com/questions/43515481/python-how-to-move-list-of-folders-with-subfolders-to-a-new-directory
+    root_dst_dir = os.path.join(os.getcwd(), "docs")
+    with cd("./examples"):
+        subprocess.run(["pycco", "-p", "-i", "-n", "4", "."])
+        root_src_dir = os.path.join(os.getcwd(), "docs")
+        for src_dir, dirs, files in os.walk(root_src_dir):
+            dst_dir = src_dir.replace(root_src_dir, root_dst_dir, 1)
+            if not os.path.exists(dst_dir):
+                os.makedirs(dst_dir)
+            for file_ in files:
+                src_file = os.path.join(src_dir, file_)
+                dst_file = os.path.join(dst_dir, file_)
+                if os.path.exists(dst_file):
+                    # in case of the src and dst are the same file
+                    if os.path.samefile(src_file, dst_file):
+                        continue
+                    os.remove(dst_file)
+                shutil.move(src_file, dst_dir)
